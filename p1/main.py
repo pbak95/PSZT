f9781f906f28d69b14f05f7966db574fd4e873a2
pbak95 <patryk.bak@gazeta.pl>
2019-04-09 19:20:10 +0200

    Dynamic part cleanup

--- p1/main.py
+++ p1/main.py
@@ -1,11 +1,11 @@
-from tester import *
-from knapsackSolver import *
+from p1.knapsackSolver import dynamic_alg, knapSack
+from p1.tester import get_test_data, easy_path, test_alg, hard_path
+# from tester import *
+# from knapsackSolver import *
 
-data = get_test_data(easy_path)
+data = get_test_data(hard_path)
 
-print("alg1")
-test_alg(first_alg, data)
-print("alg2")
-test_alg(second_alg, data)
-print("knapSack")
+print("[dynamic_alg]:")
+test_alg(dynamic_alg, data)
+print("[knapSack]:")
 test_alg(lambda W, w, c: knapSack(W, w, c, len(c)), data)
